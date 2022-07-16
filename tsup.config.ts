2ad59d99c572d320983091c5d7093af376575f53
jimmy-guzman <hi@jimmyguzman.com>
2022-07-16 16:51:55 -0500

    docs: 📝 improve docs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig((options) => {
+export default defineConfig(({ watch = false }) => {
   return {
     clean: true,
-    dts: !options.watch,
-    format: ['esm', 'cjs'],
-    minify: !options.watch,
-    sourcemap: Boolean(options.watch),
+    dts: !watch,
+    format: watch ? 'esm' : ['esm', 'cjs'],
+    minify: !watch,
+    silent: Boolean(watch),
+    sourcemap: Boolean(watch),
     target: 'node14',
   }
 })
